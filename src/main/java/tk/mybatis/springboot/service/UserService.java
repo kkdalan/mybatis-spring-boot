6c4e5a98a60ed95f7f10d6df96c4c0772c76b3e1
Alan.Huang <alan.huang@neweb.com.tw>
2018-05-02 17:05:10 +0800

    加入MVC

--- src/main/java/tk/mybatis/springboot/service/UserService.java
+++ src/main/java/tk/mybatis/springboot/service/UserService.java
@@ -1,7 +1,12 @@
 package tk.mybatis.springboot.service;
 
+import java.util.List;
+
 import tk.mybatis.simple.model.SysUser;
 
 public interface UserService {
+	
 	public SysUser findById(Long id);
+	
+	public List<SysUser> findAll();
 }
